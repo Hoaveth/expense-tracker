43c1fa4d9ff2f9593867c3dc146aebbe8fea9b4d
Hoaveth <hoaveth.gaming@gmail.com>
2023-05-27 22:25:43 +0800

    Added form with react-hook-form and zod

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3),
+  amount: z.number().min(1),
+  category: z.string().min(1),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const Form = () => {
+  const categories = ["Food", "Clothing", "Others"];
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="" className="form-label">
+          Name
+        </label>
+        <input {...register("name")} type="text" className="form-control" />
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="" className="form-label">
+          Amount
+        </label>
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="" className="form-label">
+          Category
+        </label>
+        <select {...register("category")} className="form-control">
+          {categories.map((category) => (
+            <option value={category}>{category}</option>
+          ))}
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
